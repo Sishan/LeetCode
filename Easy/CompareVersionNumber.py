0144ee1b3b833536f5b4c1156b57786618ce20b1
Sishan <Chen-0910@hotmail.com>
2016-05-29 20:45:13 -0500

    Update CompareVersionNumber.py

--- Easy/CompareVersionNumber.py
+++ Easy/CompareVersionNumber.py
@@ -1,4 +1,4 @@
-/*
+###
 Compare two version numbers version1 and version2.
 If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
 
@@ -7,9 +7,9 @@ The . character does not represent a decimal point and is used to separate numbe
 For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
 
 Here is an example of version numbers ordering:
-
 0.1 < 1.1 < 1.2 < 13.37
-*/
+###
+
 class Solution(object):
     def compareVersion(self, version1, version2):
         """
