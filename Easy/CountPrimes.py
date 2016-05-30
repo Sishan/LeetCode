b05f19f0592bfd7bc869a5e78fc4a05b2c5cb089
Sishan <Chen-0910@hotmail.com>
2016-05-29 20:43:38 -0500

    Update CountPrimes.py

--- Easy/CountPrimes.py
+++ Easy/CountPrimes.py
@@ -1,11 +1,11 @@
-/*
+###
 Description:
 Count the number of prime numbers less than a non-negative number, n
 
 References:
 How Many Primes Are There? (https://primes.utm.edu/howmany.html)
 Sieve of Eratosthenes (http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
-*/
+###
 
 class Solution(object):
     def countPrimes(self, n):
