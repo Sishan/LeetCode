e8d7f9114a1a781a95af53187802b90efca9c282
Sishan <Chen-0910@hotmail.com>
2016-05-30 22:41:55 -0500

    Create DeleteNodeInALinkedList.py

---
+++ Easy/DeleteNodeInALinkedList.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if (node == None or node.next == None):
+            return
+        next = node.next
+        node.val = next.val
+        node.next = next.next
