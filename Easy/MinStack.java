daf5dd0cee277ae84c4eeb5fad5cafbe060b5ebe
Sishan <Chen-0910@hotmail.com>
2016-06-22 14:59:39 -0500

    Update MinStack.java

--- Easy/MinStack.java
+++ Easy/MinStack.java
@@ -50,3 +50,26 @@ public class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+ 
+ /*
+Stack<T>.Peek Method:
+Returns the object at the top of the Stack<T> without removing it
+
+Because you need constant time in retrieving the min element, so you need to keep another stack to store the min element
+Can use ArrayList or LinkedList to implement the stack
+
+Hints:
+- Consider space-time tradeoff. How would you keep track of the minimums using extra space?
+- Make sure to consider duplicate elements.
+
+O(n) runtime, O(n) space – Extra stack:
+Use an extra stack to keep track of the current minimum value. During the push operation we choose the new element or the current minimum, 
+whichever that is smaller to push onto the min stack.
+
+O(n) runtime, O(n) space – Minor space optimization:
+If a new element is larger than the current minimum, we do not need to push it on to the min stack. When we perform the pop operation, 
+check if the popped element is the same as the current minimum. If it is, pop it off the min stack too.
+
+Reference:
+http://blog.csdn.net/linhuanmars/article/details/41008731
+*/
