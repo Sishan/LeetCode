8beab1f1034a3c95bd7ed293e2c644620f8e7852
Sishan <Chen-0910@hotmail.com>
2016-07-06 13:31:58 -0500

    "Create RectangleArea.java"

---
+++ Easy/RectangleArea.java
@@ -0,0 +1,26 @@
+/*
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+Assume that the total area is never beyond the maximum possible value of int.
+*/
+
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int areaOfSqrA = (C - A) * (D - B);
+        int areaOfSqrB = (G - E) * (H - F);
+
+        int left = Math.max(A,E);
+        int right = Math.min(C,G);
+        int top = Math.min(D,H);
+        int bottom = Math.max(B,F);
+
+        int overlap = 0;
+        if (right > left && top > bottom) overlap = (right - left) * (top - bottom);
+        return areaOfSqrA + areaOfSqrB - overlap;
+    }
+}
+
+/*
+Reference:
+https://leetcode.com/discuss/39398/my-java-solution-sum-of-areas-overlapped-area
+*/
