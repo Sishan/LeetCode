894cf10cea192794f264a90b53a141fe7e6db394
Sishan <Chen-0910@hotmail.com>
2016-07-06 11:17:36 -0500

    Create PowerOfTwo.py

---
+++ Easy/PowerOfTwo.py
@@ -0,0 +1,18 @@
+"""
+Given an integer, write a function to determine if it is a power of two.
+"""
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if (n <= 0):
+        	return False
+        return (n & (n-1)) == 0
+
+"""
+(x-1) & x == 0 → x是2的某次幂
+http://algorithm.yuanbin.me/math_and_bit_manipulation/o1_check_power_of_2.html
+"""
