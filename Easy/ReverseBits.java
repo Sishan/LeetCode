fa04d0b61ce6035d32dce676461d6b318c40c775
Sishan <Chen-0910@hotmail.com>
2016-07-11 16:48:35 -0500

    Update ReverseBits.java

--- Easy/ReverseBits.java
+++ Easy/ReverseBits.java
@@ -15,7 +15,54 @@ public class Solution {
     public int reverseBits(int n) {
         int res = 0;
         for (int i = 0; i < 32; i ++){
-        	if (())
+        	if (((1 << i) & n) != 0){
+        		res |= 1 << (31 - i);
+        	}
         }
+        return res;
     }
 }
+
+/*
+Similar to Reverse Integer - https://leetcode.com/problems/reverse-integer/
+
+// use string
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        String str = Integer.toBinaryString(n);
+
+        // expand to 32 bits
+        int length = str.length();
+        for (int i = 0; i < 32 - length; i++) {
+            str = "0" + str;
+        }
+
+        // convert to unsigned int
+        long result = 0;
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            result += Integer.parseInt(arr[i] + "") * Math.pow(2, i);
+        }
+
+        return (int)result;
+    }
+}
+
+// use stringbuilder
+String str=Integer.toUnsignedString(n,2);
+    StringBuilder sb=new StringBuilder();
+       for(int i=str.length(); i<32;i++){
+           sb.append("0");
+       }
+       sb.append(str);
+     return (int)  Integer.parseUnsignedInt(sb.reverse().toString(),2);
+
+// system reverse method
+Integer.reverse(n)
+
+
+Reference:
+https://leetcode.com/discuss/27484/java-solution-without-explicit-bits-manipulation
+https://leetcode.com/discuss/27577/share-my-java-code
+*/
