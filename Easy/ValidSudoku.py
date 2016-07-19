15d75ae097ddc18dcf6a46ada97ae2ba05522ae0
Sishan <Chen-0910@hotmail.com>
2016-07-19 16:55:43 -0500

    Update ValidSudoko

--- Easy/ValidSudoku.py
+++ Easy/ValidSudoku.py
@@ -13,4 +13,14 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-    def checkHelper(self, board)
+        big = set()
+        for i in xrange(9):
+        	for j in xrange(9):
+        	    if board[i][j] != '.':
+        	        cur = board[i][j]
+        	        if (i,cur) in big or (cur,j) in big or (i/3, j/3, cur) in big:
+        		    	return False
+        	        big.add((i,cur))
+        	        big.add((cur,j))
+        	        big.add((i/3, j/3, cur))
+        return True
