06cdb94bb4b98de8639f17c06ddd14db153440b1
Sishan <chen-0910@hotmail.com>
2017-03-04 10:52:19 -0600

    Update

--- Medium/CountingBits.java
+++ Medium/CountingBits.java
@@ -11,7 +11,6 @@ Space complexity should be O(n).
 Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
 */
 
-
 public class Solution {
     public int[] countBits(int num) {
         int[] res = new int[num + 1];
