c680f13053a04292258324c0b6449fc03cd018ed
Sishan <chen-0910@hotmail.com>
2017-03-08 19:47:28 -0600

    Update

--- Medium/LongestSubstringWithoutRepeatingCharacters.java
+++ Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -27,7 +27,7 @@ public class Solution {
 }
 
 /*
-the basic idea is, keep a hashmap which stores the characters in string as keys and their positions as values, 
+The basic idea is, keep a hashmap which stores the characters in string as keys and their positions as values, 
 and keep two pointers which define the max substring. move the right pointer to scan through the string , and 
 meanwhile update the hashmap. If the character is already in the hashmap, then move the left pointer to the 
 right of the same character last found. Note that the two pointers can only move forward.
