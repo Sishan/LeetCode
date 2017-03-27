4db65e9bea8e2bb394c869b08ff21b4ba1a241e7
Sishan <chen-0910@hotmail.com>
2017-03-27 16:43:32 -0500

    Update

--- Hard/NumberOfDigitOne.java
+++ Hard/NumberOfDigitOne.java
@@ -33,12 +33,12 @@ Use variables a and b to make the explanation a bit nicer.
 Go through the digit positions by using position multiplier m with values 1, 10, 100, 1000, etc.
 For each position, split the decimal representation into two parts, for example split n=3141592 into 
 a=31415 and b=92 when we're at m=100 for analyzing the hundreds-digit. And then we know that the 
-hundreds-digit of n is 1 for prefixes "" to "3141", i.e., 3142 times. Each of those times is a streak, 
+hundreds-digit of n is 1 for prefixes "0" to "3141", i.e., 3142 times. Each of those times is a streak, 
 though. Because it's the hundreds-digit, each streak is 100 long. So (a / 10 + 1) * 100 times, the 
 hundreds-digit is 1.
 
 Consider the thousands-digit, i.e., when m=1000. Then a=3141 and b=592. The thousands-digit is 1 for 
-prefixes "" to "314", so 315 times. And each time is a streak of 1000 numbers. However, since the 
+prefixes "0" to "314", so 315 times. And each time is a streak of 1000 numbers. However, since the 
 thousands-digit is a 1, the very last streak isn't 1000 numbers but only 593 numbers, for the suffixes 
 "000" to "592". So (a / 10 * 1000) + (b + 1) times, the thousands-digit is 1.
    
