c6923cf966ff5fcf92e848a783f6fb4a391f676a
Sishan <chen-0910@hotmail.com>
2017-04-03 15:16:53 -0500

    init

---
+++ Medium/SpiralMatrix.java
@@ -0,0 +1,49 @@
+/*
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
+*/
+
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
+        int rowBegin = 0, rowEnd = matrix.length - 1;
+        int colBegin = 0, colEnd = matrix[0].length - 1;
+        while (rowBegin <= rowEnd && colBegin <= colEnd){
+             // Traverse Right
+            for (int i = colBegin; i <= colEnd; i++){
+                res.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+             // Traverse down
+            for (int j = rowBegin; j <= rowEnd; j++){
+                res.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+             // Traverse Up
+            if (rowBegin <= rowEnd){
+                for (int i = colEnd; i >= colBegin; i--){
+                    res.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+            // Traverse Left
+            if (colBegin <= colEnd){
+                for (int j = rowEnd; j >= rowBegin; j--){
+                    res.add(matrix[j][colBegin]);
+                }
+                colBegin++;
+            }
+        }
+        return res;
+    }
+}
