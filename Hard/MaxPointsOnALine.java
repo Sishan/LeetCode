f4e2506cc2504b785b3d5ddfb2460d6ae30d18d9
Sishan <chen-0910@hotmail.com>
2017-11-22 00:08:49 -0600

    Comments added

--- Hard/MaxPointsOnALine.java
+++ Hard/MaxPointsOnALine.java
@@ -3,43 +3,52 @@ Given n points on a 2D plane, find the maximum number of points that lie on the 
 */
 
 /**
- * Definition for a point.
- * class Point {
- *     int x;
- *     int y;
- *     Point() { x = 0; y = 0; }
- *     Point(int a, int b) { x = a; y = b; }
- * }
- */
+* Definition for a point.
+* class Point {
+*     int x;
+*     int y;
+*     Point() { x = 0; y = 0; }
+*     Point(int a, int b) { x = a; y = b; }
+* }
+*/
 public class Solution {
     public int maxPoints(Point[] points) {
-        if (points == null) return 0;
-        if (points.length <= 2) return points.length;
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<Integer, Map<Integer, Integer>>();
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+        if (points.length <= 2) {
+            return points.length;
+        }
         int res = 0;
-        for (int i = 0; i < points.length; i++){
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<Integer, Map<Integer, Integer>>();
+        for (int i = 0; i < points.length; i++) {
             map.clear();
             int overlap = 0, max = 0;
-            for (int j = i + 1; j < points.length; j++){
+            for (int j = i + 1; j < points.length; j++) {
                 int x = points[j].x - points[i].x;
                 int y = points[j].y - points[i].y;
+
                 if (x == 0 && y == 0) {
                     overlap++;
                     continue;
                 }
-                int gcd = generateGcd(x,y);
-                if (gcd != 0){
+
+                int gcd = gcd(x, y);
+                if (gcd != 0) {
                     x /= gcd;
                     y /= gcd;
                 }
-                if (map.containsKey(x)){
-                    if (map.get(x).containsKey(y)) map.get(x).put(y, map.get(x).get(y)+1);
-                    else map.get(x).put(y,1);
-                }
-                else{
-                    Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-                    m.put(y,1);
-                    map.put(x, m);
+
+                if (map.containsKey(x)) {
+                    if (map.get(x).containsKey(y)) {
+                        map.get(x).put(y, map.get(x).get(y) + 1);
+                    } else {
+                        map.get(x).put(y, 1);
+                    }
+                } else {
+                    HashMap<Integer, Integer> pair_count = new HashMap<Integer, Integer>();
+                    pair_count.put(y, 1);
+                    map.put(x, pair_count);
                 }
                 max = Math.max(max, map.get(x).get(y));
             }
@@ -47,8 +56,12 @@ public class Solution {
         }
         return res;
     }
-    
-    public int generateGcd(int x, int y){               // return the greatest common divisor
-        return y == 0 ? x : generateGcd(y, x % y);
+
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
     }
 }
---
+++ Medium/SingleElementInASortedArray.java
@@ -0,0 +1,29 @@
+/*
+Given a sorted array consisting of only integers where every element appears twice except for one element which appears once. Find this single element that appears only once.
+
+Example 1:
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+Input: [3,3,7,7,10,11,11]
+Output: 10
+Note: Your solution should run in O(log n) time and O(1) space.
+*/
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+}
