816aaec60e93d77a30b59f94f1da990126ecbd8b
Sishan <chen-0910@hotmail.com>
2018-01-03 09:05:19 -0600

    Comments added

--- Medium/BestTimeToBuyAndSellStockWithCooldown.java
+++ Medium/BestTimeToBuyAndSellStockWithCooldown.java
@@ -35,8 +35,11 @@ class Solution {
 /*
 Define Recursion
 
-buy[i]: To make a decision whether to buy at i, we either take a rest, by just using the old decision at i - 1, or sell at/before i - 2, then buy at i, We cannot sell at i - 1, then buy at i, because of cooldown.
-sell[i]: To make a decision whether to sell at i, we either take a rest, by just using the old decision at i - 1, or buy at/before i - 1, then sell at i.
+buy[i]: To make a decision whether to buy at i, we either take a rest, 
+by just using the old decision at i - 1, or sell at/before i - 2, then buy at i, 
+We cannot sell at i - 1, then buy at i, because of cooldown.
+sell[i]: To make a decision whether to sell at i, we either take a rest, by 
+just using the old decision at i - 1, or buy at/before i - 1, then sell at i.
 So we get the following formula:
 
 buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);   
