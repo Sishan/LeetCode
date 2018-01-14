a83160250aef34fb9457d6835ed91010203ffd32
Sishan <chen-0910@hotmail.com>
2018-01-14 15:49:22 -0600

    Comments added

--- Easy/SymmetricTree.java
+++ Easy/SymmetricTree.java
@@ -106,3 +106,47 @@ Reference:
 https://yusun2015.wordpress.com/2015/01/07/symmetric-tree/
 http://blog.csdn.net/linhuanmars/article/details/23072829
 */
+
+/*
+public boolean isSymmetric(TreeNode root) {
+    if(root==null)  return true;
+    
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode left, right;
+    if(root.left!=null){
+        if(root.right==null) return false;
+        stack.push(root.left);
+        stack.push(root.right);
+    }
+    else if(root.right!=null){
+        return false;
+    }
+        
+    while(!stack.empty()){
+        if(stack.size()%2!=0)   return false;
+        right = stack.pop();
+        left = stack.pop();
+        if(right.val!=left.val) return false;
+        
+        if(left.left!=null){
+            if(right.right==null)   return false;
+            stack.push(left.left);
+            stack.push(right.right);
+        }
+        else if(right.right!=null){
+            return false;
+        }
+            
+        if(left.right!=null){
+            if(right.left==null)   return false;
+            stack.push(left.right);
+            stack.push(right.left);
+        }
+        else if(right.left!=null){
+            return false;
+        }
+    }
+    
+    return true;
+}
+*/
