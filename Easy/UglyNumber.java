a7773bd0236a0d83b0fce1a82af1620184a2bfc4
Sishan <chen-0910@hotmail.com>
2018-06-14 22:21:27 -0700

    Comments added

--- Easy/UglyNumber.java
+++ Easy/UglyNumber.java
@@ -8,19 +8,24 @@ Note that 1 is typically treated as an ugly number
 
 public class Solution {
     public boolean isUgly(int num) {
-      if (num <= 0) return false;
-      while (num != 1){
-          if (num % 2 == 0) num /= 2;
-          else if (num % 3 == 0) num /= 3;
-          else if (num % 5 == 0) num /= 5;
-          else return false;
-      }
-      return true;
+        if (num <= 0)
+            return false;
+        while (num != 1) {
+            if (num % 2 == 0)
+                num /= 2;
+            else if (num % 3 == 0)
+                num /= 3;
+            else if (num % 5 == 0)
+                num /= 5;
+            else
+                return false;
+        }
+        return true;
     }
 }
 
 /*
-http://blog.csdn.net/nisxiya/article/details/46767595
-http://blog.csdn.net/martin_liang/article/details/45692933
-http://blog.csdn.net/u010786672/article/details/44259927
-*/
+ * http://blog.csdn.net/nisxiya/article/details/46767595
+ * http://blog.csdn.net/martin_liang/article/details/45692933
+ * http://blog.csdn.net/u010786672/article/details/44259927
+ */
