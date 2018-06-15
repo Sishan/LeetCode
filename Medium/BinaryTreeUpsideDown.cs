dc8362f4b71f96d5e7fa9a5a992a3f9dad45b161
Sishan <chen-0910@hotmail.com>
2018-06-14 23:29:18 -0700

    Binary Tree Upside Down added

---
+++ Medium/BinaryTreeUpsideDown.cs
@@ -0,0 +1,61 @@
+/*
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+
+Example:
+
+Input: [1,2,3,4,5]
+
+    1
+   / \
+  2   3
+ / \
+4   5
+
+Output: return the root of the binary tree [4,5,2,#,#,3,1]
+
+   4
+  / \
+ 5   2
+    / \
+   3   1  
+Clarification:
+
+Confused what [4,5,2,#,#,3,1] means? Read more below on how binary tree is serialized on OJ.
+
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as [1,2,3,#,#,4,#,#,5].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode UpsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) 
+        {
+            return root;
+        }
+        TreeNode newRoot = UpsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
